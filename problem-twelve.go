57e64ede51b07764984b954487b8acf91b6dbcc3
gregsonian <gregsonian>
2018-05-22 07:32:50 +0100

    problem 12

---
+++ problem-twelve.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func divisors(n int64) int64 {
+	var c int64 = 0
+
+	for i := int64(1); i <= int64(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			c = c + 2
+		}
+	}
+	return c
+}
+
+func main() {
+	var i, j int64 = 0, 1
+	var next int64
+
+	for {
+		next = i + j
+		if divisors(next) > 500 {
+			fmt.Println(next)
+			break
+		}
+		i = next
+		j++
+	}
+}
